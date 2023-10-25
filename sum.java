9be03255a47812f33a68c5ca68ea4d4bbf436161
Rohit Sutar <114717436+rohitsureshsutar2000@users.noreply.github.com>
2023-10-25 23:20:50 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,13 @@
+package hello;
+import java.util.Scanner;
+public class sum {
+
+	public static void main(String[] args) {
+		Scanner ss=new Scanner(System.in);
+		int a=ss.nextInt();
+		int b=ss.nextInt();
+		System.out.println(a+b);
+
+	}
+
+}
