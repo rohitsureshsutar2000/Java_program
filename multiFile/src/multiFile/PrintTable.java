9f5a4d151bafd27d81009b13cb4b0debd513466a
Rohit Sutar <114717436+rohitsureshsutar2000@users.noreply.github.com>
2023-10-26 00:29:07 -0700

    Add files via upload

---
+++ multiFile/src/multiFile/PrintTable.java
@@ -0,0 +1,12 @@
+package multiFile;
+
+public class PrintTable {
+
+	public static void printtable(int No) {
+		for (int i=1;i<11;i++)
+		{
+			System.out.println(i + " + " + No+ "= "+ (No*i) );
+		}
+	}
+
+}
