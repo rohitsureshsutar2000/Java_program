5b6f0378778e9aa798ca01424f92fe4039d5d4e5
Rohit Sutar <114717436+rohitsureshsutar2000@users.noreply.github.com>
2023-10-26 23:49:06 -0700

    Add files via upload

---
+++ MenuDriven/src/arrayOp/TransposeM.java
@@ -0,0 +1,44 @@
+package arrayOp;
+
+import java.util.Scanner;
+
+public class TransposeM {
+
+	public static void transM() {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter how many rows");
+		int r=sc.nextInt();
+		System.out.println("Enter how many columns");
+		int c=sc.nextInt();
+		System.out.println("Enter matrix");
+		int[][] arr=new int[r][c];
+		for (int i=0;i<r;i++)
+		{
+			for (int j=0;j<c;j++)
+			{
+				arr[i][j]=sc.nextInt();
+			}
+		}
+		System.out.println("before tanspose");
+		for (int i=0;i<r;i++)
+		{
+			for (int j=0;j<c;j++)
+			{
+				System.out.print( arr[i][j] +"  ");
+			}
+			System.out.println();
+		}
+		System.out.println("After tanspose");
+		for (int i=0;i<c;i++)
+		{
+			for (int j=0;j<r;j++)
+			{
+				System.out.print( arr[j][i] +"  ");
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
