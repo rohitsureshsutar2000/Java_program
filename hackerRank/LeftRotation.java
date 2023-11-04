19a8e5b79db37c231cfe39bc471a84310e2e29d8
Rohit Sutar <114717436+rohitsureshsutar2000@users.noreply.github.com>
2023-11-04 09:47:55 -0700

    Add files via upload

---
+++ hackerRank/LeftRotation.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeftRotation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer> arr= new ArrayList<>();
+		arr.add(1);
+		arr.add(2);
+		arr.add(3);
+		arr.add(4);
+		arr.add(5);
+
+		System.out.println(testArray.rotateLeft(4,arr));
+	}
+
+}
