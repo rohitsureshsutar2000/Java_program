da98770075c68e30f148f67ffc1edf4dadc6377f
rohit sutar <rohitsureshsutar2000@gmail.com>
2024-01-12 10:53:17 +0530

    OneToOne Relation HNate

---
+++ Advance_java/EmbedableTest/src/test/java/com/demo/model/Address.java
@@ -0,0 +1,8 @@
+package com.demo.model;
+
+public class Address {
+	private int addrId;
+	private String street;
+	private String city;
+	
+}
---
+++ Advance_java/EmbedableTest/src/test/java/com/demo/model/MyUser.java
@@ -0,0 +1,10 @@
+package com.demo.model;
+
+
+public class MyUser {
+	private int id;
+	private String name;
+	private Address addr;
+	
+	
+}
